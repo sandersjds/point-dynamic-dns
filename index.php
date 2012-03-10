bb39e89efe8c36242e9432ce3dc66cf50764609f
unknown <jds@aramis.(none)>
2012-03-09 19:32:51 -0500

    markdown'd, removed slicehost references (because rackspace did too)

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
 /*
 
-USAGE: configure client machine to wget or curl this script every <interval> to keep dynamic DNS records at slicehost current
+USAGE: configure client machine to wget or curl this script every <interval> to keep dynamic DNS records at POINTHQ current
 
-http://ip.hostname.com/ - by default or with bad arguments, the script will return the current remote IP.
-http://ip.hostname.com/?h=<hostname>&r=<slicehost record ID>&i=<OPTIONAL: current host IP&p=<slicehost API password> - will return either no change necessary or will update the IP.
+here's a bash script for you:
 
+  #!/bin/bash
+  curl "http://ip.your.webhost.com/?host=pointhq&h=hostname&r=recordid&z=zoneid&u=username&p=password" -s | logger >> /dev/null 2>&1
+
+  
  
 what we want this script to do
  - script is executed from server
@@ -32,54 +35,25 @@ if ($_GET['i'] && valid_ip($_GET['i'])) { // ip address
 	$record['data']=$_SERVER['REMOTE_ADDR'];
 }
 
-($_GET['host'])?$current_host=$_GET['host']:$current_host='slicehost';
-
-if ($current_host=='slicehost') {
-	if ($hostname) {
-		$current_record_value=exec("nslookup ".escapeshellarg($hostname)." ns1.slicehost.com | grep Address | tail -1 | awk '{print $2}'");
-		if ($record) {
-			if ($current_record_value==$record['data']) {
-				exit('No update required: DNS record == current IP ('.$record['data'].')');
-			} else {
-				if ($password && $recordid) {
-					require('slicemanager.php');
-					$slice=new SliceManager($password);
-					$slice->updateRecord($recordid,$record);
-					exit('DNS record updated: '.$hostname.' == '.$record['data']);
-				} else {
-					//no password or recordid
-					exit($_SERVER['REMOTE_ADDR']);
-				}
-			}
+if ($hostname) {
+	$current_record_value=exec("nslookup ".escapeshellarg($hostname)." dns1.pointhq.com | grep Address | tail -1 | awk '{print $2}'");
+	if ($record) {
+		if ($current_record_value==$record['data']) {
+			exit('No update required: DNS record == current IP ('.$record['data'].')');
 		} else {
-			//no current IP address
-			exit($_SERVER['REMOTE_ADDR']);
+			if ($password && $recordid) {
+				print_r(exec("curl -H 'Accept: application/xml' -H 'Content-type: application/xml' http://pointhq.com/zones/".$zoneid."/records/".$recordid." -u ".$username.":".$password." -X PUT -d '<zone-record><data>".$record['data']."</data><name>".$hostname.".</name></zone-record>'"));
+				exit('DNS record updated: '.$hostname.' == '.$record['data']);
+			} else {
+				//no password or recordid
+				exit($_SERVER['REMOTE_ADDR']);
+			}
 		}
 	} else {
-		//no hostname
+		//no current IP address
 		exit($_SERVER['REMOTE_ADDR']);
 	}
-} else if ($current_host=='pointhq') {
-	if ($hostname) {
-		$current_record_value=exec("nslookup ".escapeshellarg($hostname)." dns1.pointhq.com | grep Address | tail -1 | awk '{print $2}'");
-		if ($record) {
-			if ($current_record_value==$record['data']) {
-				exit('No update required: DNS record == current IP ('.$record['data'].')');
-			} else {
-				if ($password && $recordid) {
-					print_r(exec("curl -H 'Accept: application/xml' -H 'Content-type: application/xml' http://pointhq.com/zones/".$zoneid."/records/".$recordid." -u ".$username.":".$password." -X PUT -d '<zone-record><data>".$record['data']."</data><name>".$hostname.".</name></zone-record>'"));
-					exit('DNS record updated: '.$hostname.' == '.$record['data']);
-				} else {
-					//no password or recordid
-					exit($_SERVER['REMOTE_ADDR']);
-				}
-			}
-		} else {
-			//no current IP address
-			exit($_SERVER['REMOTE_ADDR']);
-		}
-	} else {
-		//no hostname
-		exit($_SERVER['REMOTE_ADDR']);
-	}
+} else {
+	//no hostname
+	exit($_SERVER['REMOTE_ADDR']);
 }
